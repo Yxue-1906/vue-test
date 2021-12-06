31a3f2cdffabdd724827279b9e5bdcee3365c1ba
nanashi <na@na.shi>
2021-12-06 15:32:25 +0800

    deploy branch

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     proxyTable: {
       '/': {
         target: 'http://server.xypan.online:8080',
